4ee7190fa4239dd0269fb29a2f8d53a58a0064fd
falk.zeh <falk.zeh@tonies.com>
2023-04-27 11:12:50 +0200

    add airflow dag

--- airflow/dags/pipeline.py
+++ airflow/dags/pipeline.py
@@ -1,16 +1,43 @@
+from datetime import datetime, timedelta
 from airflow import DAG
-from airflow.operators.empty import EmptyOperator
-from airflow.operators.bash import BashOperator
-from datetime import datetime
+from airflow.operators.python_operator import PythonOperator
+from extract import PopcoreChallenge
 
 
-with DAG(
-    dag_id='get_covid_data',
-    start_date=datetime(2023, 4, 26),
-    schedule_interval=None
-) as dag:
-    extract = BashOperator(
-        task_id='extract',
-        bash_command='python3 extract.py',
-    )
+def run_popcore_challenge(url: str):
+    etl = PopcoreChallenge(url=url)
+    etl.execute()
 
+default_args = {
+    "owner": "airflow",
+    "depends_on_past": False,
+    "start_date": datetime(2023, 4, 27),
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": 1,
+    "retry_delay": timedelta(minutes=5),
+}
+
+dag = DAG(
+    "popcore_challenge",
+    default_args=default_args,
+    description="A DAG to run PopcoreChallenge",
+    schedule_interval=timedelta(days=1),
+    catchup=False,
+)
+
+owid_covid_data = PythonOperator(
+    task_id="owid_covid_data",
+    python_callable=run_popcore_challenge,
+    op_args=["https://covid.ourworldindata.org/data/owid-covid-data.csv"],
+    dag=dag,
+)
+
+covid_hospitalizations = PythonOperator(
+    task_id="covid_hospitalizations",
+    python_callable=run_popcore_challenge,
+    op_args=["https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/hospitalizations/covid-hospitalizations.csv"],
+    dag=dag,
+)
+
+owid_covid_data >> covid_hospitalizations
