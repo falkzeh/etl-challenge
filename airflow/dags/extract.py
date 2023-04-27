f37c4e5f69442ca37d73548cfd42f650815fa524
falk.zeh <falk.zeh@tonies.com>
2023-04-27 09:11:00 +0200

    update readme and extract class

--- airflow/dags/extract.py
+++ airflow/dags/extract.py
@@ -4,51 +4,108 @@ from sqlalchemy import create_engine
 from io import StringIO
 import os
 import datetime as dt
+from urllib.parse import urlparse
+import logging
 from dotenv import load_dotenv
 
 load_dotenv()
+logging.basicConfig(level=logging.INFO)
 
 
-def get_csv_data(url) -> pd.DataFrame:
-    """
-    Get csv data from url and return a pandas dataframe including the loadtime and source url.
+class PopcoreChallenge:
+    def __init__(
+        self,
+        url: str,
+        table_name: str = None,
+        db_user: str = os.getenv("MYSQL_USER"),
+        db_password: str = os.getenv("MYSQL_PW"),
+        db_host: str = os.getenv("MYSQL_HOST"),
+        db_database: str = os.getenv("MYSQL_DB"),
+    ) -> None:
+        """
+        Initialize PopcoreChallenge class with MySQL database credentials and url to csv file.
 
-    Args:
-        url (str): url to csv file
+        Args:
+            url (str): url to csv file
+            table_name (str): name of table in database. Defaults to None.
+            db_user (str, optional): database user. Defaults to os.getenv("MYSQL_USER").
+            db_password (str, optional): database password. Defaults to os.getenv("MYSQL_PW").
+            db_host (str, optional): database host. Defaults to os.getenv("MYSQL_HOST").
+            db_database (str, optional): database name. Defaults to os.getenv("MYSQL_DB").
+        """
+        self.url = url
+        self.table_name = table_name
+        self.db_user = db_user
+        self.db_password = db_password
+        self.db_host = db_host
+        self.db_database = db_database
 
-    Returns:
-        df (pd.DataFrame): pandas dataframe
-    """
-    response = requests.get(url)
-    csv_content = StringIO(response.text)
-    df = pd.read_csv(csv_content)
-    df["LOADTIME"] = dt.datetime.now()
-    df["SOURCE"] = url
-    return df
+    def get_table_name(self, url):
+        """
+        Get table name from url in case no table name is provided.
 
+        Args:
+            url (str): url to csv file
 
-def get_engine():
-    """
-    Get database engine.
+        Returns:
+            table_name (str): name of table in database
+        """
+        if self.table_name:
+            return self.table_name
 
-    Returns:
-        engine (sqlalchemy.engine): sqlalchemy engine
-    """
-    user = os.getenv("MYSQL_USER")
-    password = os.getenv("MYSQL_PW")
-    host = os.getenv("MYSQL_HOST")
-    database = os.getenv("MYSQL_DB")
-    url = f"mysql+pymysql://{user}:{password}@{host}:3306/{database}"
-    return create_engine(url)
+        path = urlparse(url).path
+        filename = path.split("/")[-1]
+        name_without_extension = filename.replace(".csv", "")
+        table_name = name_without_extension.replace("-", "_")
+        return table_name
 
+    def get_csv_data(self, url) -> pd.DataFrame:
+        """
+        Get csv data from url and return a pandas dataframe including the loadtime and source url.
 
-with get_engine().connect() as conn:
-    df = get_csv_data("https://covid.ourworldindata.org/data/owid-covid-data.csv")
-    df.to_sql(name="owid_covid_data", con=conn, if_exists="replace", index=False)
+        Args:
+            url (str): url to csv file
 
-    df = get_csv_data(
-        "https://github.com/owid/covid-19-data/blob/master/public/data/hospitalizations/covid-hospitalizations.csv"
-    )
-    df.to_sql(name="covid_hospitalizations", con=conn, if_exists="replace", index=False)
+        Returns:
+            df (pd.DataFrame): pandas dataframe
+        """
+        logging.info(f"Getting data from {url}")
+        response = requests.get(url)
+        csv_content = StringIO(response.text)
+        df = pd.read_csv(csv_content)
+        df["LOADTIME"] = dt.datetime.now()
+        df["SOURCE"] = url
+        logging.info(f"Data from {url} successfully loaded into dataframe")
+        return df
 
-    conn.close()
+    def get_engine(self):
+        """
+        Get database engine.
+
+        Returns:
+            engine (sqlalchemy.engine): sqlalchemy engine
+        """
+        logging.info("Getting database engine")
+        url = f"mysql+pymysql://{self.db_user}:{self.db_password}@{self.db_host}:3306/{self.db_database}"
+        logging.info("Database engine successfully loaded")
+        return create_engine(url)
+
+    def execute(self):
+        """
+        Execute the ETL process.
+        """
+        engine = self.get_engine()
+        table_name = self.get_table_name(self.url)
+        df = self.get_csv_data(self.url)
+        with engine.connect() as conn:
+            df.to_sql(
+                name=table_name,
+                con=conn,
+                if_exists="replace",
+                index=False,
+                chunksize=5000,
+            )
+            conn.close()
+            logging.info(
+                f"Data from {self.url} successfully loaded into table {table_name}"
+            )
--- readme.md
+++ readme.md
@@ -1,16 +1,32 @@
 # Popcore Challenge
 
-### Install requirements
+## Setup
+To tackle this challenge I decided to use Airflow as a workflow manager. I have used it in the past and I think it is a great tool for this kind of task. I have also used Docker to make the setup easier. For the database I decided to go with MySQL on AWS because it is a service that I am familiar with and it is easy to setup. For the deployment I used terraform in order to have a reproducible infrastructure, versioning and to be able to scale it easily if needed (Plus I can be sure to delete everything without being charged after the challenge is completed).
+
+![setup](screenshots/0_setup.png)
+
+### Airflow
+In order to get started you create a virtual environment and install the requirements.
 ```bash
 python3 -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
 ```
-
-### Start Airflow
+Then you start the Airflow server with docker compose (if you haven't installed docker compose you can find more information [here](https://docs.docker.com/compose/)). 
 ```bash
 cd airflow
 docker compose up -d
 ```
+You can access the UI at http://localhost:8080 (username and password are both 'airflow'). We can see that the server is running and that the DAG is paused.
+![airflow](screenshots/1_airflow_running.png)
 
+### MySQL
+In order to access the MySQL database you can use the credentials stored in the `.env` file. If you're for example using DataGrip you can create a new connection and it should look like this:
+![mysql](screenshots/2_datagrip.png)
+
+The two tables are in the `popcore` database:
+```sql
+select * from owid_covid_data limit 5;
+select * from covid_hospitalizations limit 5;
+```
 
